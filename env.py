3a82a0961aed10d52796211fda6f192de2af5e8b
solm4671 <144464601+solm4671@users.noreply.github.com>
2023-09-14 13:04:11 +0300

    Update env.py

--- env.py
+++ env.py
@@ -7,7 +7,7 @@ API_ID = os.getenv("API_ID", "21627756").strip()
 API_HASH = os.getenv("API_HASH", "fe77fbf0cae9f7f5ece37659e2466cf1").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6479433495:AAEf_dT0k678wrlWtJaGFQ6t1jJNL8p4_58").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/UI_XB")
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/Zz_Z_Z2")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
